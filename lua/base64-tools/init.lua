f4d3368e97ecad67b08b8bb9cf214b150d198623
Holger Klausnitzer <holger@klausnitzer.dk>
2025-02-10 15:53:44 +0100

    support macOS and Linux

--- lua/base64-tools/init.lua
+++ lua/base64-tools/init.lua
@@ -5,7 +5,15 @@ M.base64_encode = function()
   local start_pos, end_pos = vim.fn.getpos("'<"), vim.fn.getpos("'>")
   local lines = vim.api.nvim_buf_get_text(0, start_pos[2] - 1, start_pos[3], end_pos[2] - 1, end_pos[3], {})
   local text = table.concat(lines, "\n")
-  local encoded = vim.fn.system("base64 --wrap=0", text):gsub("\n", "")
+
+  -- Cross-platform compatibility
+  local encoded
+  if vim.fn.has("mac") == 1 then
+    encoded = vim.fn.system("base64 -b 0", text):gsub("\n", "")
+  else
+    encoded = vim.fn.system("base64 --wrap=0", text):gsub("\n", "")
+  end
+
   vim.api.nvim_buf_set_text(0, start_pos[2] - 1, start_pos[3], end_pos[2] - 1, end_pos[3], { encoded })
 end
 
@@ -14,7 +22,15 @@ M.base64_decode = function()
   local start_pos, end_pos = vim.fn.getpos("'<"), vim.fn.getpos("'>")
   local lines = vim.api.nvim_buf_get_text(0, start_pos[2] - 1, start_pos[3], end_pos[2] - 1, end_pos[3], {})
   local text = table.concat(lines, "\n")
-  local decoded = vim.fn.system("base64 -d", text)
+
+  -- Cross-platform compatibility
+  local decoded
+  if vim.fn.has("mac") == 1 then
+    decoded = vim.fn.system("base64 -D", text)
+  else
+    decoded = vim.fn.system("base64 -d", text)
+  end
+
   vim.api.nvim_buf_set_text(0, start_pos[2] - 1, start_pos[3], end_pos[2] - 1, end_pos[3], { decoded })
 end
 
